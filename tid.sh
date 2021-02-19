25cb03995cd6c7844856ca9b249414de74ae6c79
Benjamin Wilkins <benjamin.wilkins99@gmail.com>
2021-02-19 15:34:11 -0500

    Add script for using touch id with sudo on macos

---
+++ tid.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Enable touch ID for sudo
+
+echo "# sudo: auth account password session
+auth       sufficient     pam_tid.so
+auth       sufficient     pam_smartcard.so
+auth       required       pam_opendirectory.so
+account    required       pam_permit.so
+password   required       pam_deny.so
+session    required       pam_permit.so" > /etc/pam.d/sudo
